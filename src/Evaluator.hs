09bd2b5eecc8ed65eab3cf2866ed396dc321d9a8
biuld <xboltk@gmail.com>
2021-01-02 08:41:26 -0500

    add unary operator, fix wrong precedence

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,6 +1,7 @@
 module Evaluator where
 
 import Parser
+import Lexer
 
 eval :: Expr -> Int
 eval (Num i) = i
@@ -9,3 +10,7 @@ eval (SubOp l r) = eval l - eval r
 eval (MulOp l r) = eval l * eval r
 eval (DivOp l r) = eval l `div` eval r
 eval (Pth e) = eval e
+eval (Unary operator operand) = 
+    case operator of
+        Add -> eval operand
+        Sub -> - (eval operand)
--- src/Parser.hs
+++ src/Parser.hs
@@ -8,10 +8,16 @@ data Expr
   | SubOp Expr Expr
   | DivOp Expr Expr
   | Pth Expr
+  | Unary Syntax Expr
   | Num Int
   deriving (Show)
 
 type Precedence = Int
+
+getUnaryOpPrecedence :: Syntax -> Precedence
+getUnaryOpPrecedence Add = 3
+getUnaryOpPrecedence Sub = 3
+getUnaryOpPrecedence t = 0
 
 getBinaryOpPrecedence :: Syntax -> Precedence
 getBinaryOpPrecedence Mul = 2
@@ -31,19 +37,37 @@ parse input = tryRestart $ parseExpr (filter notSpace input) 0
   where
     tryRestart :: (Expr, [Syntax]) -> (Expr, [Syntax])
     tryRestart (l, []) = (l, [])
-    tryRestart (l, op : tail) = case parseExpr tail 0 of
+    tryRestart (l, op : tail) = case parseExpr tail (getBinaryOpPrecedence op) of
       (r, rst) -> tryRestart (buildBinaryExpr op l r, rst)
 
     parseExpr :: [Syntax] -> Precedence -> (Expr, [Syntax])
-    parseExpr xs p =
+    parseExpr xs@(h:t) p 
+      | isUnaryOperator h = parseUnary xs p
+      | otherwise = parseBinary xs p
+
+    parseBinary :: [Syntax] -> Precedence -> (Expr, [Syntax])
+    parseBinary xs p = 
       case parsePth xs of
         (l, []) -> (l, [])
         (l, ys@(op : tail)) ->
           let precedence = getBinaryOpPrecedence op
-           in if precedence > p
+            in if precedence > p
                 then case parseExpr tail precedence of
                   (r, rst) -> (buildBinaryExpr op l r, rst)
                 else (l, ys)
+
+    parseUnary :: [Syntax] -> Precedence -> (Expr, [Syntax])
+    parseUnary (operator:tail) p =
+      let precedence = getUnaryOpPrecedence operator 
+          in if precedence >= p
+            then case parseExpr tail precedence of
+              (operand, rst) -> (Unary operator operand, rst)
+            else error $ "wrong token " <> show operator
+
+    isUnaryOperator :: Syntax -> Bool
+    isUnaryOperator Add = True
+    isUnaryOperator Sub = True
+    isUnaryOperator _ = False
 
     parsePth :: [Syntax] -> (Expr, [Syntax])
     parsePth (OpenPth : tail) =
