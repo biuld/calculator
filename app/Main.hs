9422a6a76ab546b7033d7effcb75bc21beafeaa5
biuld <xboltk@gmail.com>
2021-03-04 22:29:01 +0800

    add Haskeline to offer better command line input experience

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,49 @@
 module Main where
 
+import Control.Monad.Trans
+import Data.List (isPrefixOf)
 import Evaluator
 import Lexer
 import Logger
 import Parser (parse)
+import System.Console.Haskeline
 import System.IO
 
-main :: IO ()
-main = runInterpreter False
-  where
-    runInterpreter :: Bool -> IO ()
-    runInterpreter showTree = do
-      prompt
-      input <- getLine
-      case input of
-        ":quit" -> putStrLn "goodbye!\n"
-        ":enableAST" -> do
-          putStrLn "displaying Abstract Syntax Tree\n"
-          runInterpreter True
-        ":disableAST" -> do
-          putStrLn "stop displaying Abstract Syntax Tree\n"
-          runInterpreter False
-        _ ->
-          case do
-            tokens <- lexx input
-            parse tokens of
-            Left msg -> putStrLn msg
-            Right e -> prettyPrint e showTree
-            >> runInterpreter showTree
+options = [":enableAST", ":disableAST", ":help", ":quit"]
 
-    prompt :: IO ()
-    prompt = do
-      putStr "cal> "
-      hFlush stdout
+searchFunc :: String -> [Completion]
+searchFunc str = map simpleCompletion $ filter (str `isPrefixOf`) options
+
+settings =
+  let c = completeWord Nothing " \t" $ return . searchFunc
+   in setComplete c defaultSettings
+
+main :: IO ()
+main = runInputT settings (loop False)
+  where
+    loop :: Bool -> InputT IO ()
+    loop showTree = do
+      minput <- getInputLine "cal>"
+      case minput of
+        Nothing -> loop showTree
+        Just ":quit" -> outputStrLn "goodbye! 😎\n"
+        Just ":enableAST" -> do
+          outputStrLn "displaying Abstract Syntax Tree\n"
+          loop True
+        Just ":disableAST" -> do
+          outputStrLn "stop displaying Abstract Syntax Tree\n"
+          loop False
+        Just ":help" -> do
+          outputStrLn ":enableAST => to display the parse tree"
+          outputStrLn ":disableAST => to stop displaying the parse tree"
+          outputStrLn ":help => to display this message"
+          outputStrLn ":quit => to say goodbye 👋\n"
+          loop showTree
+        Just other ->
+          let res = do
+                tokens <- lexx other
+                parse tokens
+           in case res of
+                Left msg -> outputStrLn $ msg <> "\n"
+                Right e -> lift $ prettyPrint e showTree
+                >> loop showTree
