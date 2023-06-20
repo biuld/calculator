5a6f3f416d4df2cba2dd7b0be03c942f0ac510cc
Tyler Chan <xboltk@gmail.com>
2023-06-21 01:42:15 +0800

    add more types

--- src/Language/Calculator/WASM/Printer.hs
+++ src/Language/Calculator/WASM/Printer.hs
@@ -1,6 +1,6 @@
 module Language.Calculator.WASM.Printer where
 
-import Language.Calculator.WASM.Types (Func (..), Inst (..), Module (..), Val (..), ValType (..))
+import Language.Calculator.WASM.Types (Func (..), Inst (..), Module (..), Signage (..), Val (..), ValType (..))
 
 class Show a => Wat a where
     wat :: a -> String
@@ -26,8 +26,55 @@ instance Wat Val where
     wat (F32v f32) = show f32
     wat (F64v f64) = show f64
 
+instance Wat Signage where
+    wat Sign = "s"
+    wat UnSign = "u"
+
 instance Wat Inst where
     wat (Const ty v) = wat ty <> ".const " <> wat v
+    wat (Equal ty a b) = join " " [wat (Const ty a), wat (Const ty b), wat ty <> ".eq"]
+    wat (NotEqual ty a b) = join " " [wat (Const ty a), wat (Const ty b), wat ty <> ".ne"]
+    wat (Gt ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.gt_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.gt_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.gt"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.gt"]
+    wat (Lt ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.lt_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.lt_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.lt"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.lt"]
+    wat (Ge ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.ge_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.ge_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.ge"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.ge"]
+    wat (Le ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.le_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.le_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.le"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.le"]
+    wat (Add ty a b) = join " " [wat (Const ty a), wat (Const ty b), wat ty <> ".add"]
+    wat (Sub ty a b) = join " " [wat (Const ty a), wat (Const ty b), wat ty <> ".sub"]
+    wat (Mul ty a b) = join " " [wat (Const ty a), wat (Const ty b), wat ty <> ".mul"]
+    wat (Div ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.div_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.div_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.div"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.div"]
+    wat (Rem ty si a b) = case ty of
+        I32t -> join " " [wat (Const ty a), wat (Const ty b), "i32.rem_" <> wat si]
+        I64t -> join " " [wat (Const ty a), wat (Const ty b), "i64.rem_" <> wat si]
+        F32t -> join " " [wat (Const ty a), wat (Const ty b), "f32.rem"]
+        F64t -> join " " [wat (Const ty a), wat (Const ty b), "f64.rem"]
+    wat (IfThenElse b l r) =
+        let
+            lhs = fmap wat l
+            rhs = fmap wat r
+         in
+            join " " $ [wat (Const I32t b), "if", "then"] ++ lhs ++ ["else"] ++ rhs
+    wat (Loop is) = join " " $ fmap wat is
+    wat _ = undefined
 
 instance Wat Func where
     wat f =
@@ -49,6 +96,6 @@ instance Wat Module where
             start = case m.start of
                 Just id -> wrapBlank ["start", "$" <> id]
                 Nothing -> []
-            fs = join "" $ fmap wat m.func
+            fs = join " " $ fmap wat m.func
          in
             wrapBlank [x | x <- ["module", i, fs, start], not $ null x]
--- src/Language/Calculator/WASM/Types.hs
+++ src/Language/Calculator/WASM/Types.hs
@@ -6,8 +6,30 @@ data ValType = I32t | I64t | F32t | F64t deriving (Eq, Show)
 
 data Val = I32v Int32 | I64v Int64 | F32v Float | F64v Double deriving (Eq, Show)
 
+data Signage = Sign | UnSign deriving (Eq, Show)
+
 data Inst where
     Const :: ValType -> Val -> Inst
+    Equal :: ValType -> Val -> Val -> Inst
+    NotEqual :: ValType -> Val -> Val -> Inst
+    Gt :: ValType -> Signage -> Val -> Val -> Inst
+    Ge :: ValType -> Signage -> Val -> Val -> Inst
+    Lt :: ValType -> Signage -> Val -> Val -> Inst
+    Le :: ValType -> Signage -> Val -> Val -> Inst
+    Add :: ValType -> Val -> Val -> Inst
+    Sub :: ValType -> Val -> Val -> Inst
+    Mul :: ValType -> Val -> Val -> Inst
+    Div :: ValType -> Signage -> Val -> Val -> Inst
+    Rem :: ValType -> Signage -> Val -> Val -> Inst
+    IfThenElse :: Val -> [Inst] -> [Inst] -> Inst
+    Loop :: [Inst] -> Inst
+    Local :: Id -> ValType -> Inst
+    LocalGet :: Id -> Inst
+    LocalSet :: Id -> Val -> Inst
+    LocalTee :: Id -> Val -> Inst
+    Global :: Id -> ValType -> Inst
+    GlobalGet :: Id -> Inst
+    GlobalSet :: Id -> Val -> Inst
 
 deriving instance Show Inst
 deriving instance Eq Inst
@@ -22,8 +44,8 @@ data Func = Func
     deriving (Eq, Show)
 
 data Module = Module
-    { id :: Maybe Id
+    { id :: Maybe String
     , func :: [Func]
-    , start :: Maybe Id
+    , start :: Maybe String
     }
     deriving (Eq, Show)
